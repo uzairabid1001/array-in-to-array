40cb098618728c5737c65d8dbfb59fd61185f195
uzair abid <uzairabid1001@gmial.com>
2022-09-04 20:17:06 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let car ={
+    Honda:{
+        civic:{
+            new:{
+             Model:2021,
+             color:"black",
+             price:5000000,
+            }
+        },
+    }
+
+
+}
+let carname = {...car.Honda.civic.new}
+console.log(carname)
+let arr =[
+    bike=[
+        unique=[
+            cg125=[
+                "engine70cc","model2022","rate78000"
+            ]
+        ]
+    ]
+]
+
+let bikename =[...cg125]
+console.log(bikename)
